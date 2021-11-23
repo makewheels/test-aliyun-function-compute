a6c1a0ba6f572c05ca2feaf730705e2fa94c2798
miuser <miuser@email.com>
2021-11-23 20:47:53 +0800

    init

---
+++ src/main/java/com/eg/testaliyunfc/Start.java
@@ -0,0 +1,9 @@
+package com.eg.testaliyunfc;
+
+import java.util.UUID;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID().toString().replace("-", ""));
+    }
+}
